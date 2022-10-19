e9009ceabd1609bc4f18eff780c9cf2986e25954
eyadwa <32135341+eyadwa@users.noreply.github.com>
2022-10-19 11:31:19 +0300

    profile screen

--- lib/Core/Const/ColorsApp.dart
+++ lib/Core/Const/ColorsApp.dart
@@ -5,6 +5,11 @@ class AppColors {
 
 
  static const Color AppbarColor=Color(0xFFFFE6E6);
+ static const Color themColor=Color(0xFFFFE6E6);
  static const Color blueAppbartext=Color(0xFF243B6B);
  static const Color blacktext=Color(0xFF000000);
+ static const Color SwitchColor=Color(0xFF93FFDF);
+ static const Color IconColor=Color(0xFFDADADA);
+
+
 }
