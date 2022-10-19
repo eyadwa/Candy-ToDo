90493aa556d9e916978f7ea585bb775dfa70c131
eyadwa <32135341+eyadwa@users.noreply.github.com>
2022-10-19 15:48:32 +0300

    add redux

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,17 +1,25 @@
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
 
+import 'View/Screen/Profile.dart';
 
-
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class FlutterReduxApp extends StatelessWidget {
+  FlutterReduxApp({Key? key, required this.store, required this.title})
+      : super(key: key);
+  final Store<int> store;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-
-
+    return StoreProvider(
+      store: store,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(),
+        title: title,
+        home: Profile()
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 import 'Home.dart';
 import 'View/Screen/Profile.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Candy TODO',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home:  Profile(),
-    );
+  enum Actions { Increment }
+   int counterReducer(int state, dynamic action) {
+  return action == Actions.Increment ? state + 1 : state;
   }
+
+
+
+void main() {
+  final store = Store<int>(counterReducer, initialState: 0);
+
+  runApp( FlutterReduxApp(
+    store: store
+    , title:"Candy_TODO",
+  ));
 }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Candy TODO',
+//       theme: ThemeData(
+//
+//         primarySwatch: Colors.blue,
+//       ),
+//       home:  Profile(),
+//     );
+//   }
+// }
 
